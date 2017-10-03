89539f6eaf7972431a7ed3e4c1cc99e32c3e5889
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2017-10-03 09:08:02 +0200

    to revert

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import purple from 'material-ui/colors/purple'
+import green from 'material-ui/colors/green'
+import red from 'material-ui/colors/red'
 import App from './App'
+
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+
+import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: purple, // Purple and green play nicely together.
+    secondary: {
+      ...green,
+      A400: '#00e677',
+    },
+    error: red,
+  },
+})
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App/>
+  </MuiThemeProvider>,
+  document.getElementById('root'),
+)
 registerServiceWorker()
